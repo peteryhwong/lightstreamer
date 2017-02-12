eb7451765a66534c8f62ea7392c040e6ff040e19
Peter Wong <peter.wong@travelex.com>
2017-02-12 23:05:04 +0000

    Allow TLS

--- src/Lightstreamer.hs
+++ src/Lightstreamer.hs
@@ -1,10 +1,10 @@
-module Lightstreamer 
+module Lightstreamer
     ( module X
-    , module Y 
+    , module Y
     ) where
 
 import Lightstreamer.Client as X
-import Lightstreamer.Http as Y (ConnectionSettings(..))
+import Lightstreamer.Http as Y (ConnectionSettings(..), TlsSettings(..))
 import Lightstreamer.Request as Y hiding (HttpRequest, StandardHeaders, RequestConverter
                                          , createStandardHeaders, serializeHttpRequest)
 import Lightstreamer.Streaming as Y (StreamInfo(..), StreamHandler(..))
