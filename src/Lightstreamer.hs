32b5a0d738ead3799f66cdb0b587f0cf0142a5d3
Peter Wong <peter.wong@travelex.com>
2017-02-26 22:50:17 +0000

    Expose all of Lightstreamer.Streaming

--- src/Lightstreamer.hs
+++ src/Lightstreamer.hs
@@ -7,4 +7,4 @@ import Lightstreamer.Client as X
 import Lightstreamer.Http as Y (ConnectionSettings(..), TlsSettings(..))
 import Lightstreamer.Request as Y hiding (HttpRequest, StandardHeaders, RequestConverter
                                          , createStandardHeaders, serializeHttpRequest)
-import Lightstreamer.Streaming as Y (StreamInfo(..), StreamHandler(..))
+import Lightstreamer.Streaming as Y
